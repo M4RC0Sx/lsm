777e4862eadd42f0ff9820b5f5deda0e212a62f4
M4RC0Sx <marcos3as3@gmail.com>
2022-11-12 18:42:05 +0100

    feat(lsm_file): handled a bunch of stats, closes #11

--- src/lsm_file.c
+++ src/lsm_file.c
@@ -1,11 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
 
 #include "lsm_file.h"
 
-static char *lsm_file_get_ext(lsm_file_t *lsm_file);
+#define PERM_STR_LEN 10
 
-lsm_file_t *lsm_file_create(char *file_name, char *file_path, lsm_file_type_t file_type)
+static void lsm_file_load_stats(lsm_file_t *lsm_file);
+static lsm_file_type_t lsm_file_get_type(struct stat buf_st);
+static char *lsm_file_get_ext(lsm_file_t *lsm_file);
+static char *lsm_file_get_owner(lsm_file_t *lsm_file, struct stat buf_st);
+static char *lsm_file_get_group(lsm_file_t *lsm_file, struct stat buf_st);
+static char *lsm_file_get_permissions_string(lsm_file_t *lsm_file, struct stat buf_st);
+
+lsm_file_t *lsm_file_create(char *file_name, char *file_path)
 {
     lsm_file_t *file = malloc(sizeof(lsm_file_t));
 
@@ -15,9 +26,16 @@ lsm_file_t *lsm_file_create(char *file_name, char *file_path, lsm_file_type_t fi
     file->path = malloc(strlen(file_path) + 1);
     strcpy(file->path, file_path);
 
-    file->type = file_type;
-    file->extension = lsm_file_get_ext(file);
+    /* File stats initialization */
+    file->extension = NULL;
+    file->owner = NULL;
+    file->group = NULL;
+    file->size = 0;
+    file->last_modified_timestamp = 0;
+    file->perms_str = NULL;
+    file->hard_links_count = 0;
 
+    lsm_file_load_stats(file);
     return file;
 }
 
@@ -25,7 +43,42 @@ void lsm_file_destroy(lsm_file_t *lsm_file)
 {
     free(lsm_file->name);
     free(lsm_file->path);
+    free(lsm_file->perms_str);
     free(lsm_file);
+}
+
+void lsm_file_print(lsm_file_t *lsm_file)
+{
+    printf("%s, %s, %s, %s, %s, %s, %ld, %ld, %s, %d\n", lsm_file->name, lsm_file->path, lsm_file->type == LSM_FILE ? "file" : "directory", lsm_file->extension, lsm_file->owner, lsm_file->group, lsm_file->size, lsm_file->last_modified_timestamp, lsm_file->perms_str, lsm_file->hard_links_count);
+}
+
+static void lsm_file_load_stats(lsm_file_t *lsm_file)
+{
+    struct stat buf_st;
+    if (stat(lsm_file->path, &buf_st) == -1)
+        return;
+
+    lsm_file->type = lsm_file_get_type(buf_st);
+    lsm_file->extension = lsm_file_get_ext(lsm_file);
+    lsm_file->owner = lsm_file_get_owner(lsm_file, buf_st);
+    lsm_file->group = lsm_file_get_group(lsm_file, buf_st);
+    lsm_file->size = buf_st.st_size;
+    lsm_file->last_modified_timestamp = buf_st.st_mtime;
+    lsm_file->perms_str = lsm_file_get_permissions_string(lsm_file, buf_st);
+    lsm_file->hard_links_count = buf_st.st_nlink;
+}
+
+static lsm_file_type_t lsm_file_get_type(struct stat buf_st)
+{
+    lsm_file_type_t file_type = UNKNOWN;
+    if (S_ISDIR(buf_st.st_mode))
+        file_type = LSM_DIRECTORY;
+    else if (S_ISREG(buf_st.st_mode))
+        file_type = LSM_FILE;
+    else
+        file_type = UNKNOWN;
+
+    return file_type;
 }
 
 static char *lsm_file_get_ext(lsm_file_t *lsm_file)
@@ -38,4 +91,43 @@ static char *lsm_file_get_ext(lsm_file_t *lsm_file)
         return ext + 1;
     else
         return NULL;
+}
+
+static char *lsm_file_get_owner(lsm_file_t *lsm_file, struct stat buf_st)
+{
+    if (lsm_file->type == UNKNOWN)
+        return NULL;
+
+    struct passwd *pw = getpwuid(buf_st.st_uid);
+    return pw ? pw->pw_name : NULL;
+}
+
+static char *lsm_file_get_group(lsm_file_t *lsm_file, struct stat buf_st)
+{
+    if (lsm_file->type == UNKNOWN)
+        return NULL;
+
+    struct group *gr = getgrgid(buf_st.st_gid);
+    return gr ? gr->gr_name : NULL;
+}
+
+static char *lsm_file_get_permissions_string(lsm_file_t *lsm_file, struct stat buf_st)
+{
+    if (lsm_file->type == UNKNOWN)
+        return NULL;
+
+    char *perm_str = (char *)malloc((PERM_STR_LEN + 1) * sizeof(char));
+    perm_str[0] = (S_ISDIR(buf_st.st_mode)) ? 'd' : '-';
+    perm_str[1] = (buf_st.st_mode & S_IRUSR) ? 'r' : '-';
+    perm_str[2] = (buf_st.st_mode & S_IWUSR) ? 'w' : '-';
+    perm_str[3] = (buf_st.st_mode & S_IXUSR) ? 'x' : '-';
+    perm_str[4] = (buf_st.st_mode & S_IRGRP) ? 'r' : '-';
+    perm_str[5] = (buf_st.st_mode & S_IWGRP) ? 'w' : '-';
+    perm_str[6] = (buf_st.st_mode & S_IXGRP) ? 'x' : '-';
+    perm_str[7] = (buf_st.st_mode & S_IROTH) ? 'r' : '-';
+    perm_str[8] = (buf_st.st_mode & S_IWOTH) ? 'w' : '-';
+    perm_str[9] = (buf_st.st_mode & S_IXOTH) ? 'x' : '-';
+    perm_str[10] = '\0';
+
+    return perm_str;
 }
